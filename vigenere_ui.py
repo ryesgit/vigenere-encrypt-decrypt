e08893894b7ddd2bfe1ead3587e6314b94932f82
User Rye <128949576+ryesgit@users.noreply.github.com>
2023-04-02 17:49:45 +0800

    Adjust font for readability

--- vigenere_ui.py
+++ vigenere_ui.py
@@ -9,7 +9,7 @@ layout = [
     [sg.Text('Enter text to encode or decode'), sg.Input(border_width=5, key='input')],
     [sg.T(' ', size=(1,1))],
     [sg.Text('Key: '), sg.Input(pad=(10, 0), border_width=5, key='key')],
-    [sg.Text('', key='decode-or-encode'), sg.Text(key='value')],
+    [sg.Text('', key='decode-or-encode', font=('Roboto', 20)), sg.Text(key='value', font=('Roboto', 20))],
     [sg.Button('Decrypt'), sg.Button('Encrypt')],
     [sg.Button('Quit')]
 ]
