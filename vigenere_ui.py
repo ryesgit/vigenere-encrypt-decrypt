b7aa04537ffae8a0c0372f38881488ff97f546cb
User Rye <128949576+ryesgit@users.noreply.github.com>
2023-04-02 18:28:01 +0800

    Swap decrypt and encrypt buttons

--- vigenere_ui.py
+++ vigenere_ui.py
@@ -10,7 +10,7 @@ layout = [
     [sg.T(' ', size=(1,1))],
     [sg.Text('Key: '), sg.Input(pad=(10, 0), border_width=5, key='key')],
     [sg.Text('', key='decode-or-encode', font=('Roboto', 20)), sg.Text(key='value', font=('Roboto', 20))],
-    [sg.Button('Decrypt'), sg.Button('Encrypt')],
+    [sg.Button('Encrypt'), sg.Button('Decrypt')],
     [sg.Button('Quit')]
 ]
 
